bb5202c816d087be03aaba9eb75c3d3aebdc86f3
Augustseverinandersen <augustseverinandersen@hotmail.com>
2023-04-30 16:15:48 +0200

    code done

--- setup.sh
+++ setup.sh
@@ -1,3 +1,19 @@
+
+
+# python3 src/testscript.py --filepath "../../images" --train_sample_size 10000 --val_sample_size 2000 --test_sample_size 2000 --epochs 10
+
 #!/usr/bin/env bash
 
+# Create virtual enviroment 
+python3 -m venv assignment_3
+
+# Activate virtual enviroment 
+source ./assignment_3/bin/activate 
+
+# requirements
+pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+
+# Deactivate the virtual environment.
+deactivate
--- src/testscript.py
+++ src/testscript.py
@@ -42,11 +42,11 @@ sys.path.append(".")
 def input_parse():
     # initialize the parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filepath", type=str) # argument is filepath as a string
-    parser.add_argument("--train_sample_size", type = int, default = 91166)
-    parser.add_argument("--val_sample_size", type = int, default = 7500)
-    parser.add_argument("--test_sample_size", type = int, default = 7500)
-    parser.add_argument("--epochs", type = int, default = 10)
+    parser.add_argument("--filepath", type=str, help = "Path to folder images, not including metadata, train, val, and test") # argument is filepath as a string
+    parser.add_argument("--train_sample_size", type = int, default = 91166, help = "Sample size of training images, default is all images")
+    parser.add_argument("--val_sample_size", type = int, default = 7500, help = "Sample size of validation images, default is all images")
+    parser.add_argument("--test_sample_size", type = int, default = 7500, help = "Sample size of test images, default is all images")
+    parser.add_argument("--epochs", type = int, default = 10, help = "How many epochs, default is 10")
     args = parser.parse_args()
 
     return args
@@ -57,21 +57,23 @@ def input_parse():
 
 def loading_metadata(args):
     print("Loading metadata to dataframes")
-    test_metadata = pd.read_json(os.path.join( args.filepath, "metadata", "test_data.json"), lines=True) # Loading in JSON files of metadata
+    test_metadata = pd.read_json(os.path.join(args.filepath, "metadata", "test_data.json"), lines=True) # Loading in JSON files of metadata
     train_metadata = pd.read_json(os.path.join(args.filepath, "metadata", "train_data.json"), lines=True)
     val_metadata = pd.read_json(os.path.join(args.filepath, "metadata", "val_data.json"), lines=True)
     return test_metadata, train_metadata, val_metadata
 
 
-def sampling(test_metadata, train_metadata, val_metadata):
-    test_metadata = test_metadata.sample(1000)
-    train_metadata = train_metadata.sample(2000)
-    val_metadata = val_metadata.sample(1000)
+def sampling(test_metadata, train_metadata, val_metadata, args):
+    print("sampling")
+    test_metadata = test_metadata.sample(args.test_sample_size)
+    train_metadata = train_metadata.sample(args.train_sample_size)
+    val_metadata = val_metadata.sample(args.val_sample_size)
     return test_metadata, train_metadata, val_metadata
 
 
 
 def image_data_generator():
+    print("Creating Image data generator")
     # Data augmentaion 
     # ImageDataGenerator from tensorflow 
     datagen = ImageDataGenerator(horizontal_flip=True, # Flip it horizontally around the access
@@ -84,14 +86,16 @@ def image_data_generator():
 
 
 def image_directory(args):
-    main_dir = os.path.dirname(args.filepath)
-    directory_images = main_dir # removing the last part of the specified filepath
+    print("Finding path to images")
+    directory_images = os.path.dirname(args.filepath)
+    # removing the last part of the specified filepath
     return directory_images
 
 
 
 
-def training_images(train_metadata, directory_images):
+def training_images(datagen, train_metadata, directory_images):
+    print("Finding training images: ")
     train_tf = datagen.flow_from_dataframe( # using keras flow  from dataframe 
         dataframe = train_metadata, # Defining dataframe 
         directory = directory_images, # Path to images 
@@ -107,13 +111,13 @@ def training_images(train_metadata, directory_images):
     return train_tf
 
 
-def val_images(val_metadata, directory_images):
+def val_images(datagen, val_metadata, directory_images):
+    print("Finding validation images: ")
     val_tf = datagen.flow_from_dataframe(
         dataframe = val_metadata,
         directory = directory_images,
         x_col = "image_path",
         y_col = "class_label",
-        #subset = "validation",
         target_size=(224, 224),
         color_mode="rgb",
         class_mode = "categorical",
@@ -127,6 +131,7 @@ def val_images(val_metadata, directory_images):
 
 
 def test_image_generator():
+    print("Creating test image data generator")
     test_datagen = ImageDataGenerator(
                                     rescale = 1./255. # datagenerator for test, it only has to rescale the images 
     )
@@ -136,7 +141,8 @@ def test_image_generator():
 
 
 
-def test_images(test_metadata, directory_images):
+def test_images(test_datagen, test_metadata, directory_images):
+    print("Finding test images")
     test_tf = test_datagen.flow_from_dataframe(
         dataframe = test_metadata,
         directory = directory_images,
@@ -147,12 +153,13 @@ def test_images(test_metadata, directory_images):
         batch_size = 128,
         shuffle = False # do not shuffle the images 
     )
+    return test_tf
 
 
 
 
-
-def load_model():  
+def load_model():
+    print("Loading model: ")  
     # load model without classifier layers
     model = VGG16(include_top=False, 
                 pooling='avg',
@@ -193,7 +200,7 @@ def load_model():
 
 
 
-def train_model(train_tf, val_tf, args):
+def train_model(train_tf, val_tf, args, model):
     print("Training model")
     # fit the data generator to our images
 
@@ -236,10 +243,10 @@ def plot_history(H, epochs, save_path):
 
 
 
-def plot_model_history(H, args, save_path):
+def plot_model_history(H, args):
     print("Saving plots")
-    save_path = os.path.join("..", "figs")
-    plot_history(H, 10, save_path)
+    save_path = os.path.join("figs")
+    plot_history(H, args.epochs, save_path)
 
 
 
@@ -247,8 +254,8 @@ def plot_model_history(H, args, save_path):
 
 
 
-def prediction(test_tf, train_tf):
-    print("Test model with test data")
+def predictions(test_tf, train_tf, model):
+    print("Testing model with test images")
     # Predict the label of the test_images
     pred = model.predict(test_tf)
     pred = np.argmax(pred,axis=1)
@@ -256,8 +263,8 @@ def prediction(test_tf, train_tf):
     # Map the label 
     labels = (train_tf.class_indices)
     labels = dict((v,k) for k,v in labels.items())
-    pred1 = [labels[k] for k in pred]
-    return pred, pred1
+    pred = [labels[k] for k in pred]
+    return pred
     # Display the result
     #print(f'The first 5 predictions: {pred[:20]}')
 
@@ -266,31 +273,46 @@ def prediction(test_tf, train_tf):
 
 
 def classification_report_function(test_metadata, pred):
-    print("Classification Report: ")
     y_true = test_metadata['class_label'].values
     classification_report_test = classification_report(y_true, pred)
     print("Classification Report - Test Data:")
     print(classification_report_test)
+    return classification_report_test
 
+def saving_model(model):
+    print("Saving model")
+    folder_path = os.path.join("models", "image_classification.keras") # Defining out path
+    tf.keras.models.save_model( # Using Tensor Flows function for saving models.
+    model, folder_path, overwrite=True, save_format=None 
+    ) # Model name, folder, Overwrite existing saves, save format = none 
 
+def metrics_save_function(classifier_metrics):
+    print("Saving classifier metrics")
+    folder_path = os.path.join("models")
+    file_name = "classifier_metrics.txt"
+    file_path = os.path.join(folder_path, file_name)
+
+    with open(file_path, "w") as f: # "Writing" the classifier metrics, thereby saving it.
+        f.write(classifier_metrics)
+    print("Reports saved")
 
 def main_function():
   args = input_parse()
   test_metadata, train_metadata, val_metadata = loading_metadata(args)
-  test_metadata, train_metadata, val_metadata = sampling(test_metadata, train_metadata, val_metadata)
+  test_metadata, train_metadata, val_metadata = sampling(test_metadata, train_metadata, val_metadata, args)
   datagen = image_data_generator()
-  directory_images = image_directory()
-  train_tf = training_images(train_metadata, directory_images)
-  val_tf = val_images(val_metadata, directory_images)
+  directory_images = image_directory(args)
+  train_tf = training_images(datagen, train_metadata, directory_images)
+  val_tf = val_images(datagen, val_metadata, directory_images)
   test_datagen = test_image_generator()
-  test_tf = test_images(test_metadata, directory_images)
+  test_tf = test_images(test_datagen, test_metadata, directory_images)
   model = load_model()
-  H = train_model(train_tf, val_tf, args)
-  plot_history(H, epochs)
-  plot_model_history(H, args, save_path)
-  pred, pred1 = predictions(test_tf, train_tf)
-  classification_report_function(test_metadata, pred)
-
+  H = train_model(train_tf, val_tf, args, model)
+  plot_model_history(H, args)
+  pred = predictions(test_tf, train_tf, model)
+  classification_report_test = classification_report_function(test_metadata, pred)
+  saving_model(model)
+  metrics_save_function(classification_report_test)
 
 # filepath "..", "..", "..", "images"
 
